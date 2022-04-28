9af5d12956a01fd4872df79a23abe163893e6c84
Ballard Ingram <ballardingram@outlook.com>
2022-04-28 12:39:38 -0500

    saving point 13.3.6 - create api routes for post

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// CREATE > ASSOCIATIONS 'ONE TO MANY' 'USER FILE CREATES MANY USERS'
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// CREATE > ASSOCIATIONS ' ONE TO ONE' 'POST BELONGS TO ONE USER'
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
